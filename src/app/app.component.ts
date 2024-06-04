73d94490f9585ea9ee3c1574f674dd2ce9c671c5
August Schau <schaua@augustschau.com>
2024-06-04 17:15:58 -0400

    Add ProjectContainerComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProjectsContainerComponent } from "./projects/projects-container/projects-container.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ProjectsContainerComponent]
 })
 export class AppComponent {
   title = 'awesome';
