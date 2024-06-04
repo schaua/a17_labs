9d087a928c79c952fb6a03dd4b99068fa36f79be
August Schau <schaua@augustschau.com>
2024-06-04 17:25:00 -0400

    Creating Data Structures and displaying JSON

--- src/app/projects/projects-container/projects-container.component.ts
+++ src/app/projects/projects-container/projects-container.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MOCK_PROJECTS } from '../shared/mock-projects';
+import { Project } from '../shared/project.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-projects-container',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './projects-container.component.html',
   styleUrl: './projects-container.component.css'
 })
-export class ProjectsContainerComponent {
+export class ProjectsContainerComponent implements OnInit {
+  projects: Project[] = MOCK_PROJECTS;
+  constructor(){}
+  ngOnInit(): void {
+  }
 
 }
