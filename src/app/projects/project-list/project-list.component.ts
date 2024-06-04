0f14deb32b20f8e2745049d6c0d3f506ec72f6df
August Schau <schaua@augustschau.com>
2024-06-04 17:52:08 -0400

    Added ProjectFormComponent.

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from '../shared/project.model';
 import { ProjectCardComponent } from "../project-card/project-card.component";
+import { ProjectFormComponent } from "../project-form/project-form.component";
 
 @Component({
     selector: 'app-project-list',
     standalone: true,
     templateUrl: './project-list.component.html',
     styleUrl: './project-list.component.css',
-    imports: [ProjectCardComponent]
+    imports: [ProjectCardComponent, ProjectFormComponent]
 })
 export class ProjectListComponent implements OnInit {
   @Input()
