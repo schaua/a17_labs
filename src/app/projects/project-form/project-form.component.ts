f5e8ad1d5379c3146988f0c3ea9c9b9954a5b108
August Schau <schaua@augustschau.com>
2024-06-04 18:55:32 -0400

    Configure FormControls and binding.

--- src/app/projects/project-form/project-form.component.ts
+++ src/app/projects/project-form/project-form.component.ts
@@ -1,18 +1,29 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Project } from '../shared/project.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-project-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './project-form.component.html',
   styleUrl: './project-form.component.css'
 })
 export class ProjectFormComponent implements OnInit {
+  @Input()
+  project!: Project;
   @Output()
   cancel = new EventEmitter<void>();
-
+  projectForm: FormGroup = new FormGroup({});
   constructor() {}
   ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      name: new FormControl(this.project.name),
+      description: new FormControl(this.project.description),
+      budget: new FormControl(this.project.budget),
+      isActive: new FormControl(this.project.isActive)
+    });
   }
   onCancelClick(event: Event) {
     event.preventDefault();
