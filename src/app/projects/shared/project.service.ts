5a0da782e78a1a6a3ab65bf1fcf6a481d40d79b8
August Schau <schaua@augustschau.com>
2024-06-05 10:26:01 -0400

    HTTP Get to populate application data.

--- src/app/projects/shared/project.service.ts
+++ src/app/projects/shared/project.service.ts
@@ -1,14 +1,17 @@
+import { environment } from '../../../environments/environments';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { MOCK_PROJECTS } from './mock-projects';
 import { Project } from './project.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-  constructor() { }
+  private projectsUrl= environment.backendUrl + '/projects/';
+  constructor(private http: HttpClient) { }
   list(): Observable<Project[]>{
-    return of(MOCK_PROJECTS);
+    return this.http.get<Project[]>(this.projectsUrl);
   }
 }
